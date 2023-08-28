e1264e6958fa52c3314b191e4d7e6777dd0f4780
Sandu-tt <tharakasanu80@gmail.com>
2023-08-28 11:15:26 +0530

    updated placeorderserlvet

--- Back_End/src/lk/ijse/servlet/PurchaseOrderServletAPI.java
+++ Back_End/src/lk/ijse/servlet/PurchaseOrderServletAPI.java
@@ -1,8 +1,6 @@
 package lk.ijse.servlet;
 
-import javax.json.Json;
-import javax.json.JsonArrayBuilder;
-import javax.json.JsonObjectBuilder;
+import javax.json.*;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,7 +14,6 @@ import java.sql.*;
  * @created 27/08/2023
  * @project JavaEE_POS_App
  */
-
 @WebServlet(urlPatterns = {"/pages/purchase"})
 public class PurchaseOrderServletAPI extends HttpServlet {
     @Override
@@ -68,7 +65,7 @@ public class PurchaseOrderServletAPI extends HttpServlet {
 
                     JsonArrayBuilder allOrders = Json.createArrayBuilder();
                     while (rst.next()) {
-                        String code = rst.getString(1);
+                        String  code = rst.getString(1);
                         String id = rst.getString(2);
                         String date = rst.getString(3);
                         String subTotal = rst.getString(4);
@@ -115,8 +112,8 @@ public class PurchaseOrderServletAPI extends HttpServlet {
                         String price = rst.getString(7);
 
                         JsonObjectBuilder orderDetailObject = Json.createObjectBuilder();
-                        orderDetailObject.add("oid", oid);
-                        orderDetailObject.add("cusId", cusId);
+                        orderDetailObject.add("code", code);
+                        orderDetailObject.add("id", oid);
                         orderDetailObject.add("cusName", cusName);
                         orderDetailObject.add("code", code);
                         orderDetailObject.add("description", description);
@@ -136,6 +133,93 @@ public class PurchaseOrderServletAPI extends HttpServlet {
                 break;
         }
 
+    }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        JsonReader reader = Json.createReader(req.getReader());
+        JsonObject jsonObject = reader.readObject();
+        String oid = jsonObject.getString("oid");
+        String cusId = jsonObject.getString("cusID");
+        String date = jsonObject.getString("date");
+        String subTotal = jsonObject.getString("subTotal");
+        String discount = jsonObject.getString("discount");
+
+        String name = jsonObject.getString("name");
+        String description = jsonObject.getString("description");
+
+        resp.addHeader("Access-Control-Allow-Origin", "*");
+
+        resp.addHeader("Content-Type", "application/json");
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/company", "root", "1234");
+
+
+            connection.setAutoCommit(false);
+
+            PreparedStatement pstm = connection.prepareStatement("insert into Orders values(?,?,?,?,?)");
+            pstm.setString(1, oid);
+            pstm.setString(2, cusId);
+            pstm.setString(3, date);
+            pstm.setDouble(4, Double.parseDouble(subTotal));
+            pstm.setInt(5, Integer.parseInt(discount));
+            if (!(pstm.executeUpdate() > 0)) {
+                connection.rollback();
+                connection.setAutoCommit(true);
+                throw new RuntimeException("Order Issue");
+            } else {
+                JsonArray orderDetails = jsonObject.getJsonArray("orderDetails");
+
+                for (JsonValue orderDetail : orderDetails) {
+                    String code = orderDetail.asJsonObject().getString("code");
+                    String qty = orderDetail.asJsonObject().getString("qty");
+                    String price = orderDetail.asJsonObject().getString("price");
+
+
+                    PreparedStatement pstms = connection.prepareStatement("insert into OrderDetails values(?,?,?,?,?,?,?)");
+                    pstms.setObject(1, code);
+                    pstms.setObject(2, cusId);
+                    pstms.setObject(3, name);
+                    pstms.setObject(4, code);
+                    pstms.setObject(5, description);
+                    pstms.setObject(6, qty);
+                    pstms.setObject(7, price);
+                    if (!(pstms.executeUpdate() > 0)) {
+                        connection.rollback();
+                        connection.setAutoCommit(true);
+                        throw new RuntimeException("Order Details Issue");
+                    }
+                }
+
+                connection.commit();
+                connection.setAutoCommit(true);
+
+                JsonObjectBuilder response = Json.createObjectBuilder();
+                response.add("state", "Success");
+                response.add("message", "Order Successfully Purchased..!");
+                response.add("data", "");
+                resp.setStatus(200);
+                resp.getWriter().print(response.build());
+            }
+
+        } catch (SQLException | ClassNotFoundException | RuntimeException e) {
+            JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+            objectBuilder.add("state", "Error");
+            objectBuilder.add("message", e.getMessage());
+            objectBuilder.add("data", "");
+            resp.setStatus(500);
+            resp.getWriter().print(objectBuilder.build());
+        }
+    }
+
+
+
+    @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.addHeader("Access-Control-Allow-Origin", "*");
+        resp.addHeader("Access-Control-Allow-Methods", "PUT");
+        resp.addHeader("Access-Control-Allow-Headers", "content-type");
     }
 }
